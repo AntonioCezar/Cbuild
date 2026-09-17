8c3d761f93ed8b1ed27cd449cf8d6464cc48e7de
HenriqueDeAssuncao <henriqueestudante16@gmail.com>
2026-09-17 14:19:07 -0300

    adiciona função clean genérica no clean.sh

--- cbuild_funcs/clean.sh
+++ cbuild_funcs/clean.sh
@@ -4,6 +4,90 @@
 #clean() apaga os arquivos de build
 #cleanAll() apaga os arquivos de build e apaga os logs
 
+clean() {
+    #recebe o parâmetro diretório
+    dir=$1
+
+    #verifico se o diretório  existe
+    if [[ ! -d "$dir" ]]; then
+        echo "Diretório '$dir'  Não Existe." >> "$out_text"
+        echo "Diretório '$dir' não existe."
+        return 1
+    fi
+
+    if [[ $debug_mode == true ]]; then 
+        echo "Debug: Programa verificou se o diretório '$dir' existe." 
+    fi
+
+    #verifico se tenho permissões para acessar o diretório
+    if [[ ! -r "$dir" || ! -x "$dir" || ! -w "$dir" ]]; then
+        echo "Sem Permissão Para Acessar o Conteúdo Da Pasta '$dir'." >> "$text"
+        echo "Erro: permissão negada ao acessar '$dir'."
+        return 1
+    fi
+
+    if [[ $debug_mode == true ]]; then 
+        echo "Debug: Programa verificou se existe permissão para acessar '$dir'." 
+    fi
+
+    #verifica se o diretório não está vazio
+    if [[ -n "$(find "$dir" -mindepth 1 -print -quit)" ]]; then
+
+        if [[ $debug_mode == true ]]; then 
+            echo "Debug: Programa concluiu que o diretório '$dir' não está vazio." 
+        fi
+
+        #apaga os arquivos, se houver erro, manda para o $out_text
+        find "$dir" -mindepth 1 -delete 2>> "$out_text"
+
+        if [[ $verbose_mode == true ]]; then 
+            echo "Verboso: Programa apagou os arquivos da pasta '$dir'."
+        fi
+
+        #verifica se a pasta ficou vazia mesmo
+        if [[ -z "$(find "$dir" -mindepth 1 -print -quit)" ]]; then
+            echo "Os Arquivos Da Pasta '$dir' Foram Apagados Com Sucesso." >> $out_text
+            echo "Os arquivos da pasta '$dir' foram apagados com sucesso."
+            return 0
+        else
+            echo "Não foi possível apagar os arquivos de '$dir'. Acesse o log para mais informações."
+            return 1
+        fi
+
+
+    else 
+        if [[ $debug_mode == true ]]; then 
+            echo "Debug: Programa concluiu que o diretório '$dir' está vazio." 
+        fi
+
+        echo "Não Há Nenhum Arquivo Na Pasta '$dir' Para Apagar!" >> $out_text
+        echo "Não há arquivos na pasta '$dir' para apagar."
+        return 0
+    fi
+}
+
+
+cleanAll() {
+    #apaga os arquivos de build
+    clean "$build_dir"
+
+    #status 0 ou 1 da execução de clean
+    exit_code=$?
+
+    if [[ $verbose_mode == true ]]; then 
+        echo "Verboso: Programa iniciou a limpeza em '$logs_dir'."
+    fi
+
+    #só apaga os logs se foi possível apagar os arguivos de build
+    if [[ $exit_code -eq 0 ]]; then
+        clean "$logs_dir"
+    else 
+        echo "Falha Ao Limpar '$build_dir'. Os Logs Foram Preservados." >> $out_text
+        echo "Falha ao limpar '$build_dir'. Os logs foram preservados."
+        return 1
+    fi
+}
+
 #deixa o parâmetro do usuário em caixa baixa
 clean_mode="${1,,}"
 
@@ -13,165 +97,21 @@ out_text=$(mktemp -p "$command_log_dir" 01_clean.XXXXXX)
 build_dir="./build"
 logs_dir="./logs"
 
-clean() {
-
-    if [[ $verbose_mode == true ]]; then 
-        echo "Verboso: Programa inicou a limpeza dos arquivos temporários"
-        echo ""
-        echo "======== Limpeza em Andamento ========"
-        echo ""
-    fi
-
-    if [[ $verbose_mode == true ]]; then 
-        echo "Verboso: Programa iniciou a limpeza em'$build_dir'."
-    fi
-
-    #verifico se o diretório build existe
-    if [[ ! -d "$build_dir" ]]; then
-        echo "Diretório '$build_dir'  Não Existe." >> "$out_text"
-        echo "Diretório '$build_dir' não existe."
-        return 1
-    fi
-
-    if [[ $debug_mode == true ]]; then 
-        echo "Debug: Programa verificou se o diretório '$build_dir' existe." 
-    fi
-
-    #verifico se não tenho permissões para acessar o diretório build
-    if [[ ! -r "$build_dir" || ! -x "$build_dir" || ! -w "$build_dir" ]]; then
-        echo "Sem Permissão Para Acessar o Conteúdo Da Pasta '$build_dir'." >> "$out_text"
-        echo "Erro: permissão negada ao acessar '$build_dir'."
-        return 1
-    fi
-
-    if [[ $debug_mode == true ]]; then 
-        echo "Debug: Programa verificou se existe permissão para acessar '$build_dir'." 
-    fi
-
-    #verifica se o diretório build não está vazio
-    if [[ -n "$(find "$build_dir" -mindepth 1 -print -quit)" ]]; then 
-
-        if [[ $debug_mode == true ]]; then 
-            echo "Debug: Programa concluiu que o diretório '$build_dir' não está vazio." 
-        fi
-
-        find "$build_dir" -mindepth 1 -delete 2>> "$out_text"
-
-        #verifica se a pasta build ficou vazia mesmo
-        if [[ -z "$(find "$build_dir" -mindepth 1 -print -quit)" ]]; then 
-
-            if [[ $verbose_mode == true ]]; then 
-                echo "Verboso: Programa apagou os arquivos da pasta '$build_dir'."
-            fi
-
-            echo "Os Arquivos Da Pasta '$build_dir' Foram Apagados Com Sucesso." >> $out_text
-            echo "Os arquivos build foram apagados com sucesso."
-            return 0
-        else
-            echo "Não foi possível apagar os arquivos de '$build_dir'. Acesse o log para mais informações." 
-            return 1
-        fi
-
-    else 
-
-        if [[ $debug_mode == true ]]; then 
-            echo "Debug: Programa concluiu que o diretório '$build_dir' está vazio." 
-        fi
-
-        echo "Não Há Nenhum Arquivo Na Pasta '$build_dir' Para Apagar!" >> $out_text
-        echo "Não há arquivos na pasta '$build_dir' para apagar."
-        return 0
-    fi
-}
-
-cleanAll() {
-    #apaga os arquivos de build
-    clean
-
-    exit_code=$?
-
-    if [[ $verbose_mode == true ]]; then 
-        echo "Verboso: Programa iniciou a limpeza em'$logs_dir'."
-    fi
-
-    #só apaga os logs se foi possível apagar os arguivos de build
-    if [[ $exit_code -eq 0 ]]; then
-
-        if [[ $debug_mode == true ]]; then 
-            echo "Debug: Programa verificou se a limpeza do diretório '$build_dir' foi bem-sucedida." 
-        fi
-
-        #verifico se o diretório logs existe
-        if [[ ! -d "$logs_dir" ]]; then
-            echo "Diretório '$logs_dir'  Não Existe." >> "$out_text"
-            echo "Diretório '$logs_dir' não existe."
-            return 1
-        fi
-
-        if [[ $debug_mode == true ]]; then 
-            echo "Debug: Programa verificou se o diretório '$logs_dir' existe." 
-        fi
-
-        #verifico se tenho permissões para acessar o diretório logs
-        if [[ ! -r "$logs_dir" || ! -x "$logs_dir" ]]; then
-            echo "Sem Permissão Para Acessar o Conteúdo Da Pasta '$logs_dir'." >> "$out_text"
-            echo "Erro: permissão negada ao acessar '$logs_dir'."
-            return 1
-        fi
-
-        if [[ $debug_mode == true ]]; then 
-            echo "Debug: Programa verificou se existe permissão para acessar '$build_dir'." 
-        fi
-
-        #verifica se o diretório logs não está vazio
-        if [[ -n "$(find "$logs_dir" -mindepth 1 -print -quit)" ]]; then
-
-            if [[ $debug_mode == true ]]; then 
-                echo "Debug: Programa concluiu que o diretório '$logs_dir' não está vazio." 
-            fi
-
-            #apaga os logs, se houver erro, manda para o $out_text
-            find "$logs_dir" -mindepth 1 -delete 2>> "$out_text"
-
-            if [[ $verbose_mode == true ]]; then 
-                echo "Verboso: Programa apagou os logs da pasta '$logs_dir'."
-            fi
-
-            #verifica se a pasta logs ficou vazia mesmo
-            if [[ -z "$(find "$logs_dir" -mindepth 1 -print -quit)" ]]; then
-                echo "Os Arquivos Da Pasta '$logs_dir' Foram Apagados Com Sucesso." >> $out_text
-                echo "Os logs foram apagados com sucesso."
-                return 0
-            else
-                echo "Não foi possível apagar os arquivos de '$logs_dir'. Acesse o log para mais informações."
-                return 1
-            fi
-
-
-        else 
-            if [[ $debug_mode == true ]]; then 
-                echo "Debug: Programa concluiu que o diretório '$logs_dir' está vazio." 
-            fi
-
-            echo "Não Há Nenhum Arquivo Na Pasta '$logs_dir' Para Apagar!" >> $out_text
-            echo "Não há arquivos na pasta '$logs_dir' para apagar."
-            return 0
-        fi
-    
-    else 
-        echo "Falha Ao Limpar '$build_dir'. Os Logs Foram Preservados." >> $out_text
-        echo "Falha ao limpar '$build_dir'. Os logs foram preservados."
-        return 1
-    fi
-
-}
+if [[ $verbose_mode == true ]]; then 
+    echo "Verboso: Programa inicou a limpeza dos arquivos temporários"
+    echo ""
+    echo "======== Limpeza em Andamento ========"
+    echo ""
+fi
 
 case "$clean_mode" in
     "all" )
+        #limpa build e logs
         cleanAll
         ;;
     "" )
-        clean
+        #limpa build
+        clean "$build_dir"
         ;;
     *)
         echo "O parâmetro -$clean_mode- não existe."
